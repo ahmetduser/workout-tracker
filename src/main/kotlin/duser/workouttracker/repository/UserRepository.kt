c2f7681ca4d4bd759184d24df828b82e73c6d541
ahmet duser <ahmetduser2000@gmail.com>
2026-03-23 23:44:58 +0300

    feat: add user repository layer

---
+++ src/main/kotlin/duser/workouttracker/repository/UserRepository.kt
@@ -0,0 +1,7 @@
+package duser.workouttracker.repository
+
+import duser.workouttracker.domain.User
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface UserRepository : JpaRepository<User, UUID>
