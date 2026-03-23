48d4b2b3965086312df53bb844fab94975d6cc7f
ahmet duser <ahmetduser2000@gmail.com>
2026-03-23 23:45:07 +0300

    feat: add set entry repository layer

---
+++ src/main/kotlin/duser/workouttracker/repository/SetEntryRepository.kt
@@ -0,0 +1,9 @@
+package duser.workouttracker.repository
+
+import duser.workouttracker.domain.SetEntry
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface SetEntryRepository : JpaRepository<SetEntry, UUID> {
+    fun findAllByWorkoutSessionIdOrderBySetOrderAsc(workoutSessionId: UUID): List<SetEntry>
+}
