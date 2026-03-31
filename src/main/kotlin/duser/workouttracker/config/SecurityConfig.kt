66fc219b86a9ffd16108da08ccec41dbce02311b
ahmet duser <ahmetduser2000@gmail.com>
2026-03-31 16:32:12 +0300

    feat: basic OAuth2 login is integrated

--- src/main/kotlin/duser/workouttracker/config/SecurityConfig.kt
+++ src/main/kotlin/duser/workouttracker/config/SecurityConfig.kt
@@ -1,19 +1,13 @@
 package duser.workouttracker.config
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
+import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.crypto.factory.PasswordEncoderFactories
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-@EnableConfigurationProperties(SecurityProperties::class)
 class SecurityConfig {
 
     @Bean
@@ -22,31 +16,10 @@ class SecurityConfig {
             .csrf { it.disable() }
             .authorizeHttpRequests {
                 it
+                    .requestMatchers(HttpMethod.GET, "/").permitAll()
                     .requestMatchers(HttpMethod.GET, "/api/**", "/actuator/**").permitAll()
                     .anyRequest().authenticated()
-            }
-            .httpBasic { }
-            .formLogin { it.disable() }
-
+            }.oauth2Login(withDefaults())
         return http.build()
-    }
-
-    @Bean
-    fun userDetailsService(
-        securityProperties: SecurityProperties,
-        passwordEncoder: PasswordEncoder,
-    ): UserDetailsService {
-        val user = User.builder()
-            .username(securityProperties.username)
-            .password(passwordEncoder.encode(securityProperties.password))
-            .roles(securityProperties.role)
-            .build()
-
-        return InMemoryUserDetailsManager(user)
-    }
-
-    @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
     }
 }
